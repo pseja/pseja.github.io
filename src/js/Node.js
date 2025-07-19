a87f4132853c39daf832c7c5aa52ffbacca63704
Lukáš Pšeja <luk4sps3j4@gmail.com>
2025-07-19 20:07:36 +0200

    node rotation (swap from circles to rectangles for better visualization)

--- src/js/Node.js
+++ src/js/Node.js
@@ -1,25 +1,46 @@
 import { randomBetween } from "./utils.js";
 
 export class Node {
+    minSize = 1;
+    maxSize = 5;
+    rotationMultiplier = this.maxSize * 0.1;
+
     constructor(ctx) {
         this.ctx = ctx;
         this.x = Math.random() * window.innerWidth;
         this.y = Math.random() * window.innerHeight;
         this.vx = (Math.random() - 0.5) * 0.5;
         this.vy = (Math.random() - 0.5) * 0.5;
-        this.radius = randomBetween(1, 5);
+        this.radius = randomBetween(this.minSize, this.maxSize);
+        this.rotation = 0;
+        this.rotationSpeed =
+            Math.sqrt(this.vx ** 2 + this.vy ** 2) *
+            (this.rotationMultiplier / this.radius);
     }
 
     draw() {
-        this.ctx.beginPath();
-        this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        this.ctx.save();
+        this.ctx.translate(this.x, this.y);
+        this.ctx.rotate(this.rotation);
+
+        // this.ctx.beginPath();
+        // this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         this.ctx.fillStyle = "#888";
-        this.ctx.fill();
+        this.ctx.fillRect(
+            -this.radius,
+            -this.radius,
+            this.radius * 2,
+            this.radius * 2
+        );
+        // this.ctx.fill();
+
+        this.ctx.restore();
     }
 
     update(width, height) {
         this.x += this.vx;
         this.y += this.vy;
+        this.rotation += this.rotationSpeed;
 
         if (this.x < 0) {
             this.x = width;
